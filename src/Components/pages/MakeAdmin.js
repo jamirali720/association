c167e2d934d53ebc5bffdab39724ead0dc28161d
jamirali <jamirali720@gmail.com>
2023-01-11 20:51:39 +0600

    changed file

--- src/Components/pages/MakeAdmin.js
+++ src/Components/pages/MakeAdmin.js
@@ -1,49 +1,70 @@
-import React from 'react';
-import '../Global/Global.css'
+import React from "react";
+import "../Global/Global.css";
 
-import {useForm} from 'react-hook-form';
+import { useForm } from "react-hook-form";
+
+const divStyle = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "column",
+  width: "100%",
+  height: "100vh",
+};
 const MakeAdmin = () => {
-    const {handleSubmit, reset, register, formState: {errors}} = useForm();
-    const onSubmit = (data) => {
-       
-        const fd = new FormData();
-        fd.append('email', data.email);
-        fetch('https://association-server.onrender.com/addAdmin' , {
-            method: "POST",          
-            body: fd
-        })
-        .then(res => {
-            console.log(res)
-            if(res.ok){
-                alert('New admin has been added successfully')
-                reset();
-            }
-        })
-        .catch(error => {
-            if(error.message) {
-                alert('Something is wrong')
-            }
-        })
-    }
-    return (
-        <div className="bg-warning  m-auto rounded-3 make-admin" style={{marinTop: 150}}>
-            <div className="form-container">
-                <div className="header py-3">
-                    <h2> Make an admin </h2>
-                </div>
-                <div className="form-group pb-3">
-                  <form onSubmit={handleSubmit(onSubmit)}>
-                  <input type="email" className="form-control w-75 m-auto" {...register('email', {required: true})} placeholder="Enter Your Email" />
-                  {errors.email && <span className="text-danger"> Email is Required </span>}
-                    <button type="submit" className="btn btn-success form-control w-75 m-auto my-3 ">
-                       Submit
-                    </button>
-                  </form>
-                </div>
-            </div>
+  const {
+    handleSubmit,
+    reset,
+    register,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    const fd = new FormData();
+    fd.append("email", data.email);
+    fetch("https://association-server.onrender.com/addAdmin", {
+      method: "POST",
+      body: fd,
+    })
+      .then((res) => {
+        if (res.ok) {
+          alert("New admin has been added successfully");
+          reset();
+        }
+      })
+      .catch((error) => {
+        if (error.message) {
+          alert("Something is wrong");
+        }
+      });
+  };
+  return (
+    <div className="mainContainer bg-info" style={divStyle}>
+      <div className="form-container bg- p-5">
+        <div className="header py-3">
+          <h2> Make an admin </h2>
         </div>
-    );
+        <div className="form-group pb-3">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+              type="email"
+              className="form-control m-auto"
+              {...register("email", { required: true })}
+              placeholder="Enter Your Email"
+            />
+            {errors.email && (
+              <span className="text-danger"> Email is Required </span>
+            )}
+            <button
+              type="submit"
+              className="btn btn-success form-control d-block  m-auto my-3 "
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MakeAdmin;
-
